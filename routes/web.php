c665532f25352b8e0de0e5aca240a0f44e7ca4d7
Marwan Essa <155342240+MarwanEssa2006@users.noreply.github.com>
2026-04-09 21:15:49 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ServiceController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// بدل ما يفتح صفحة الـ welcome، هيروح علطول للـ index بتاعة الخدمات
+Route::get('/', [ServiceController::class, 'index']);
